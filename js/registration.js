ef2cb4165de80249fefb11d0fb0597282651e99e
tanulo <tanulo@barossgabor.local>
2025-03-24 09:08:42 +0100

    registration function added

--- js/registration.js
+++ js/registration.js
@@ -26,9 +26,23 @@ async function register() {
         console.log(response);
         const data = await response.json();
         console.log(data);
-        
-        
-        
+
+        if (response.ok) {
+            alert(data.message);
+            window.location.href = '../html/login.html';
+        } else if (data.errors) {
+            let errorMessages = '';
+            data.errors.forEach(error => {
+                errorMessages += `${error.error}\n`;
+            });
+            alert(errorMessages);
+        } else if (data.error) {
+            alert(data.error);
+        } else {
+            alert('Ismeretlen hiba!');
+        }
+
+
     } catch (error) {
         console.log(error);
     }
